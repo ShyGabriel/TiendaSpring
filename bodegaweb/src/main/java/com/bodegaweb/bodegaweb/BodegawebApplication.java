9a03b01999734a7f1446aae59f8943f5ab175bed
LAB-USR-LCENTRO <LAB-USR-LCENTRO@grupoutp.net>
2026-08-26 20:37:10 -0500

    Primer commit

---
+++ bodegaweb/src/main/java/com/bodegaweb/bodegaweb/BodegawebApplication.java
@@ -0,0 +1,13 @@
+package com.bodegaweb.bodegaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodegawebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodegawebApplication.class, args);
+	}
+
+}
---
+++ bodegaweb/src/test/java/com/bodegaweb/bodegaweb/BodegawebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bodegaweb.bodegaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodegawebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
